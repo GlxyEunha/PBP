23a61e4170aaee755ce5c7ad65e20a3546ed1301
GlxyEunha <112673554+GlxyEunha@users.noreply.github.com>
2022-09-04 16:38:56 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kuis PBP</title>
+        <style>
+            .start {
+                border: solid;
+                text-align: center;
+                border-radius: 8px;
+            }
+
+            .quit {
+                border: solid;
+                text-align: center;
+                border-radius: 8px;
+            }
+
+            body {
+                background:url('background2.png');
+                height: 100vh;
+                display:grid;
+                place-content:center; 
+            }
+
+            .topnav a {
+                color: #f2f2f2;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                font-size: 17px;
+            }
+
+            .topnav a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <h1>KUIS PBP</h1>
+            <h3>Anggota Kelompok:</h3>
+            <h3>1. Ahmad Isa Ibrahim</h3>
+            <h3>2. Sheva Ivanda Pratama</h3>
+            <h3>3. Muhammad Fathur Rozzaq</h3>
+            <h3>4. Bagaskara Rifki</h3>
+            <h3>5. Arka</h3>
+            <h3>6. Pikri</h3><br/>
+        </div>
+        <div class="topnav">
+            <a class="start" href="PBP18.php">START</a>
+            <a class="quit" href="www.google.com">QUIT</a>
+        </div>
+    </body>
+</html>
