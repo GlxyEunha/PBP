67a7cf9591337ac3f1b08c1a5c9fb00af5d7ce67
ahmadisa00 <102651494+ahmadisa00@users.noreply.github.com>
2022-09-05 09:30:45 +0700

    Update home.php

--- home.php
+++ home.php
@@ -45,10 +45,10 @@
             <h3>3. Muhammad Fathur Rozzaq</h3>
             <h3>4. Bagaskara Rifki</h3>
             <h3>5. Gregorius Arkadio Rajendra</h3>
-            <h3>6. Muhammad Fikri ALlfaqih</h3><br/>
+            <h3>6. Muhammad Fikri Alfqih</h3><br/>
         </div>
         <div class="topnav">
-            <a class="start" href="PBP18.php">START</a>
+            <a class="start" href="PBP 1.8.php">START</a>
             <a class="quit" href="www.google.com">QUIT</a>
         </div>
     </body>
